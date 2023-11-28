83f41242ff12ecddb0a492cca8d00725e5133130
BeccaHurtado <rebeccah7.rh@gmail.com>
2023-11-27 21:48:48 -0700

    Got controller functions to work properly.

--- FocusFlow/src/main/java/com/taskapp/FocusFlow/controller/TaskController.java
+++ FocusFlow/src/main/java/com/taskapp/FocusFlow/controller/TaskController.java
@@ -1,52 +1,49 @@
 package com.taskapp.FocusFlow.controller;
 
-import org.apache.catalina.connector.Response;
+import java.util.*;
+//import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taskapp.FocusFlow.model.Task;
-import com.taskapp.FocusFlow.service.TaskService;
+import com.taskapp.FocusFlow.repo.TaskRepository;
+//import com.taskapp.FocusFlow.service.TaskService;
 
 @RestController
-@RequestMapping(value = "/task", produces = "application/json")
 public class TaskController {
 
-	@Autowired(required = false)
-    TaskService taskService;
-
-    @PostMapping("/add-task")
-    Response addTask(@RequestBody Task task) {
-        Response res = taskService.addTask(task);
-        return res;
-    }
-
-    @PostMapping("/edit-task/{taskId}")
-    Response editPost(@PathVariable("taskId") String taskId, @RequestBody Task task) {
-        Response res = taskService.editTask(taskId, task);
-        return res;
-    }
-
-    @DeleteMapping("/delete-task/{taskId}")
-    Response deleteTask(@PathVariable("taskId") String taskId) {
-        Response res = taskService.deleteTask(taskId);
-        return res;
-    }
-
-    @GetMapping("/get-all-task")
-    Response getAllTask() {
-        Response res = taskService.getAllTask();
-        return res;
-    }
-
-    @GetMapping("/get-task-by-id/{taskId}")
-    Response getTaskById(@PathVariable("taskId") String taskId) {
-        Response res = taskService.getTaskById(taskId);
-        return res;
-    }
+	@Autowired
+    private TaskRepository taskRepo;
+	
+	@GetMapping(value = "/")
+	public List<Task> getAllTask(){
+		return taskRepo.findAll();
+	}
+	
+	@GetMapping(value = "/{taskId}")
+	public Task getTaskById(@PathVariable String taskId) {
+		return taskRepo.getTaskById(taskId);
+	}
+	
+	@PostMapping(value = "/create")
+	public Task addTask(@RequestBody Task task) {
+		return taskRepo.save(task);
+	}
+	
+	@PutMapping(value = "/update/{taskId}") 
+	public Task updateTask(@PathVariable String taskId, @RequestBody Task task) {
+		return taskRepo.save(task);
+	}
+	
+	@DeleteMapping(value = "/delete/{taskId}")
+	public void deleteTask(@PathVariable String taskId) {
+		taskRepo.deleteById(taskId);
+	}
 }
--- FocusFlow/src/main/java/com/taskapp/FocusFlow/repo/TaskRepository.java
+++ FocusFlow/src/main/java/com/taskapp/FocusFlow/repo/TaskRepository.java
@@ -10,6 +10,6 @@ import com.taskapp.FocusFlow.model.Task;
 @Repository
 public interface TaskRepository extends MongoRepository <Task, String> {
   @Query("{_id: '?0'}")
-  Task findByTaskId(String id);
+  Task getTaskById(String taskId);
     
 }
